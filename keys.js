708fa0c1b7da80344aad1958dda5058a1c50b921
jmartinez <jcmartinez1925@gmail.com>
2019-08-13 22:36:47 -0700

    set up needs and APIs

--- keys.js
+++ keys.js
@@ -1,6 +1,14 @@
-console.log('this is loaded');
+//console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+    id: process.env.OMDB_ID
+};
+
+exports.bandsintown = {
+    id: process.env.BANDSINTOWN_ID
 };
--- liri.js
+++ liri.js
@@ -1,5 +1,20 @@
+// Set up "needs" and APIs
 require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+var omdb = (keys.omdb);
+var bandsintown = (keys.bandsintown);
+
+var request = require("request");
+var moment = require("moment");
+var fs = require("fs");
+
+// User input
+var command = process.argv[2];
+var input = process.argv.slice(3).join(" ");
+
+// Set up commands
