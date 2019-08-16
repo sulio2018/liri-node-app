fa82fb5e37d63adf863b9b3cd681a958463fe0e2
jmartinez <jcmartinez1925@gmail.com>
2019-08-16 16:28:46 -0700

    complete functions

--- liri.js
+++ liri.js
@@ -18,23 +18,26 @@ var command = process.argv[2];
 var input = process.argv.slice(3).join(' ');
 
 // Set up commands
-switch (command) {
-    case 'spotify-this-song':
-        spotifyThisSong();
-        break;
-
-    case 'movie-this':
-        movieThis();
-        break;
-
-    case 'concert-this':
-        concertThis();
-        break;
-
-    case 'do-what-it-says':
-        doWhatItSays();
-        break;
-}
+function userData (command, input) {
+    switch (command) {
+        case 'spotify-this-song':
+            spotifyThisSong();
+            break;
+    
+        case 'movie-this':
+            movieThis();
+            break;
+    
+        case 'concert-this':
+            concertThis();
+            break;
+    
+        case 'do-what-it-says':
+            doWhatItSays(input);
+            break;
+    }
+};
+userData(command, input);
 
 // Create command-input functions
 function spotifyThisSong() {
@@ -45,33 +48,33 @@ function spotifyThisSong() {
         song = input;
     }
 
-    console.log(`********************`);
+    console.log(`********************\n`);
     console.log(`Your song info:`)
 
     spotify.search({ type: 'track', query: song }, function (error, data) {
         if (!error) {
-            console.log(`Song: ${data.tracks.items[0].name}`);
+            console.log(`\nSong: ${data.tracks.items[0].name}`);
             console.log(`Artist(s): ${data.tracks.items[0].artists[0].name}`);
             console.log(`Album: ${data.tracks.items[0].album.name}`);
-            console.log(`Preview Link: ${data.tracks.items[0].external_urls.spotify}`);
+            console.log(`Preview Link: ${data.tracks.items[0].external_urls.spotify}\n`);
 
             var songInfo =
             `\nspotify-this-song found: \n
             Song: ${data.tracks.items[0].name} \n
             Artist(s): ${data.tracks.items[0].artists[0].name} \n
             Album: ${data.tracks.items[0].album.name}\n
-            Preview Link: ${data.tracks.items[0].external_urls.spotify} \n********************`
+            Preview Link: ${data.tracks.items[0].external_urls.spotify} \n********************\n`
 
             fs.appendFile('log.txt', songInfo, function (error) {
                 if (error) throw error;
             });
 
         } else {
-            return console.log(error);
+            return console.log('Song not found.');
         }
 
     });
-}
+};
 
 /////////////////////////////////////////////////////////
 function movieThis() {
@@ -85,19 +88,20 @@ function movieThis() {
         movie = input;    
     }
 
-    console.log(`********************`);
+    console.log(`********************\n`);
     console.log(`Your movie info:`);
 
-    request('http://www.omdbapi.com/?t=' + movie + '&apikey=4d47306d', function (error, response, body) {
+    request('http://www.omdbapi.com/?t=' + movie + '&apikey=4d47306d', 
+    function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log(`Movie Title: ${JSON.parse(body).Title}`);
+            console.log(`\nMovie Title: ${JSON.parse(body).Title}`);
             console.log(`Release Year: ${JSON.parse(body).Year}`);
             console.log(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
             console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`);
             console.log(`Country: ${JSON.parse(body).Country}`);
             console.log(`Language: ${JSON.parse(body).Language}`);
             console.log(`Plot: ${JSON.parse(body).Plot}`);
-            console.log(`Actor(s): ${JSON.parse(body).Actors}`);
+            console.log(`Actor(s): ${JSON.parse(body).Actors}\n`);
 
             var movieInfo =
             `\nmovie-this found: \n
@@ -108,18 +112,18 @@ function movieThis() {
             Country:${JSON.parse(body).Country} \n
             Language: ${JSON.parse(body).Language} \n
             Plot: ${JSON.parse(body).Plot} \n
-            Actor(s): ${JSON.parse(body).Actors} \n********************`
+            Actor(s): ${JSON.parse(body).Actors} \n********************\n`
 
             fs.appendFile('log.txt', movieInfo, function (error) {
                 if (error) throw error;
             });
 
         } else {
-            return console.log(error);
+            return console.log('Movie not found.');
         }
 
     });
-}
+};
 
 //////////////////////////////////////////////////////
 function concertThis() {
@@ -131,33 +135,55 @@ function concertThis() {
         artist = input;
     }
 
-    console.log(`********************`);
-    console.log(`Your artist info:`);
+    console.log(`********************\n`);
+    console.log(`Your concert info:`);
 
-    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", 
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=" + bandsintown, 
     function (error, response, body) {
-        if (!error && response.statusCode === 200) {         
-            console.log(`Venue: ${JSON.parse(body).venue.name}`);
-            console.log(`Venue City: ${JSON.parse(body).venue.city}`, 
-            `Venue Country: ${JSON.parse(body).venue.country}`);
+        if (!error && response.statusCode === 200) {
+            var userArtist = JSON.parse(body);
+            if (userArtist.length > 0) {
+                for (i = 0; i < 1; i++) {
 
-            // var concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
-            // console.log(`Date and Time: ${concertDate}\n`);
+                    console.log(`\nArtist: ${userArtist[i].lineup[0]}`); 
+                    console.log(`Venue: ${userArtist[i].venue.name}`);
+                    console.log(`Venue Location: ${userArtist[i].venue.latitude},${userArtist[i].venue.longitude}`);
+                    console.log(`Venue City, Country: ${userArtist[i].venue.city}, ${userArtist[i].venue.country}`);
 
-            var artistInfo = 
-            `\n concert-this found: \n
-            Venue: ${JSON.parse(body).venue.name} \n
-            Venue City: ${JSON.parse(body).venue.city} \n
-            Venue Country: ${JSON.parse(body).venue.country} \n********************`
+                    var concertDate = moment(userArtist[i].datetime).format("MM/DD/YYYY hh:00 A");
+                    console.log(`Date and Time: ${concertDate}\n`);
 
-            fs.appendFile('log.txt', artistInfo, function (error) {
-                if (error) throw error;
-            });
+                    var concertInfo =
+                    `\nconcert-this found: \n
+                    Artist: ${userArtist[i].lineup[0]} \n
+                    Venue: ${userArtist[i].venue.name} \n
+                    Venue Location: ${userArtist[i].venue.latitude},${userArtist[i].venue.longitude} \n
+                    Venue City: ${userArtist[i].venue.city}, ${userArtist[i].venue.country} \n********************\n`
 
-        } else {
+                    fs.appendFile('log.txt', concertInfo, function (error) {
+                        if (error) throw error;
+                    });
+                };
+
+            } else {
+                console.log('Concert not found!');
+            };
+        };
+    });
+};
+
+//////////////////////////////////////////////
+function doWhatItSays() {
+    fs.readFile('random.txt', 'utf8', function (error, data) {
+        if (error) {
             return console.log(error);
         }
 
-    });
-}
+        var dataArray = data.split(',');
 
+        command = dataArray[0];
+        input = dataArray[1];
+
+        userData(command, input);
+    });
+};
